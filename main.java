7d0e7dc1e375b1f482daa1ef7cb78b019aac397c
kannan2410 <kannandurvas@gmail.com>
2026-03-15 16:07:12 +0530

    java practice DSA

---
+++ main.java
@@ -0,0 +1,16 @@
+//basic structure in java
+class main{
+    // public static void main(String[] args){
+    //     System.out.println("Hello world");
+    // }
+
+    public static void main(String[] args) {
+        String Name = "Dhanush";
+        int Age = 35;
+        System.out.println("Name : " + Name);
+        System.out.println("Age : " + Age);
+    }
+}
+
+
+
---
+++ stack.java
@@ -0,0 +1,41 @@
+class stack {
+    static int[] stack = new int[100];
+    static int top = -1;
+
+    public static void push(int value){
+        top = top + 1;
+        stack[top] = value;
+    }
+
+    public static int pop(){
+        int remove = stack[top];
+        top = top - 1;
+        return remove;
+    }
+
+    public static void main(String[] args) {
+        push(10);
+        push(20);
+        push(30);
+        System.out.print("[");
+        for(int i=0; i<=top;i++){
+            if(i==top){
+                System.out.print(stack[i]);
+            }else{
+                System.out.print(stack[i]+",");
+            }
+        }
+        System.out.print("]");
+        System.out.println();
+        System.out.println("remove value: "+ pop());
+        System.out.print("[");
+        for(int i=0; i<=top;i++){
+            if(i==top){
+                System.out.print(stack[i]);
+            }else{
+                System.out.print(stack[i]+",");
+            }
+        }
+        System.out.print("]");
+    }
+}
