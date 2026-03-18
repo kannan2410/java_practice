0fd0d1d66d68710999cbda0d14bbc9cd3f18218d
kannan2410 <kannandurvas@gmail.com>
2026-03-18 15:14:51 +0530

    Valid parentheses

---
+++ stack2.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class stack2 {
+
+    public static boolean isValid(String str){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else if(ch == ')' || ch == '}' || ch == ']'){
+
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if((ch == ')' && top != '(') ||
+                   (ch == '}' && top != '{') ||
+                   (ch == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "{[()]}";
+        System.out.println(isValid(input));
+    }
+}
